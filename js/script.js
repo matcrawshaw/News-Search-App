cb34dfdd1dbd1c0eb7326c160f1664db2d41b1b6
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-02-03 10:14:02 +0000

    JS wasn't reading the userSearch input value. Refactored the code so now it does.

--- js/script.js
+++ js/script.js
@@ -1,44 +1,36 @@
-let userSearch = $("#userSearch").val().trim();
 let searchButton = $("#searchButton");
-let numArticles = 
+let numArticles =
 
 
-$(searchButton).on("click", function (e) { 
-    e.preventDefault();
+    $(searchButton).on("click", function (e) {
+        e.preventDefault();
 
-articlesCont = $("#articlesCont")
+        articlesCont = $("#articlesCont")
 
+        let userSearch = $("#userSearch").val().trim()
 
+        console.log(userSearch);
 
-
-
-
-
-const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearch + "&api-key=ftsh42q2Uw4BCIx7TcwJd9qxcABol0Gh"
+        const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearch + "&api-key=ftsh42q2Uw4BCIx7TcwJd9qxcABol0Gh"
 
 
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-let results = response.response.docs;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            let results = response.response.docs;
 
- console.log(results);
-  
- 
+            console.log(results);
 
+            for (var i = 0; i < results.length; i++) {
+                const articleTitle = $("<h3>").text(results[i].headline.main);
+                console.log(results[i].headline.main);
+                articlesCont.append(articleTitle);
+            }
 
-
-    for (var i = 0; i < results.length; i++) {
-const articleTitle = $("<h3>").text(results[i].headline.main);
-
-articlesCont.append(articleTitle)
+        });
 
     }
-    
-});
-
-}
-)
+    )
